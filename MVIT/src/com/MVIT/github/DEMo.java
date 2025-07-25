cfa03be41dcdb3ff412ef7b3475090d990f340c2
your-github-username <prathishs.2004@gmail.com>
2025-07-25 14:17:27 +0530

    Update

--- MVIT/src/com/MVIT/github/DEMo.java
+++ MVIT/src/com/MVIT/github/DEMo.java
@@ -2,7 +2,7 @@ package com.MVIT.github;
 
 public class DEMo {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Github");
+		System.out.println("Welcome to github");
 	}
 
 }
